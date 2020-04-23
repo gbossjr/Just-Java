a4e5785388fea79abbac39366928b1323c2b75ca
Geoffrey Bosire Jr <gbosirejr@gmail.com>
2020-04-23 10:31:19 -0800

    updated submitOrder method

--- app/src/main/java/android/example/justjava/MainActivity.java
+++ app/src/main/java/android/example/justjava/MainActivity.java
@@ -22,7 +22,10 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        displayPrice(quantity * 5);
+        int price = quantity * 5;
+        String priceMessage = "Total: $" + price;
+        priceMessage = priceMessage + "\nThank You!";
+        displayMessage(priceMessage);
     }
 
     /**
@@ -49,5 +52,13 @@ public class MainActivity extends AppCompatActivity {
     public void decrement(View view) {
         quantity = quantity - 1;
         display(quantity);
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 }
