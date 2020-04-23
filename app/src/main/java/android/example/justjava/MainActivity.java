fde48c4e52d2b0db85012bc69b4c4965dcc3433a
Geoffrey Bosire Jr <gbosirejr@gmail.com>
2020-04-23 06:37:45 -0800

    added increment and decrement button and methods

--- app/src/main/java/android/example/justjava/MainActivity.java
+++ app/src/main/java/android/example/justjava/MainActivity.java
@@ -20,8 +20,9 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(2);
-        displayPrice(2*5);
+        int quantity = 2;
+        display(quantity);
+        displayPrice(quantity * 5);
     }
 
     /**
@@ -38,5 +39,15 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    public void incrememt(View view) {
+        int quantity = 3;
+        display(quantity);
+    }
+
+    public void decrement(View view) {
+        int quantity = 1;
+        display(quantity);
     }
 }
