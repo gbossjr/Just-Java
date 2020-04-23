e370919e1f6fdc30b691ed660d5fd4f06f82f163
Geoffrey Bosire Jr <gbosirejr@gmail.com>
2020-04-23 08:48:06 -0800

    added nested view to the layout/design

--- app/src/main/java/android/example/justjava/MainActivity.java
+++ app/src/main/java/android/example/justjava/MainActivity.java
@@ -10,6 +10,8 @@ import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
+    int quantity = 2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,8 +22,6 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int quantity = 2;
-        display(quantity);
         displayPrice(quantity * 5);
     }
 
@@ -42,12 +42,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void incrememt(View view) {
-        int quantity = 3;
+        quantity = quantity + 1;
         display(quantity);
     }
 
     public void decrement(View view) {
-        int quantity = 1;
+        quantity = quantity - 1;
         display(quantity);
     }
 }
